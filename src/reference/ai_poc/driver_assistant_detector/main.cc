02ac5bdfa1cd7fde852d02cef6462ea66cf7e148
Misha Brazhnikov <br.misha@gmail.com>
2025-10-03 22:43:39 +0300

    removed old code from detector

--- src/reference/ai_poc/driver_assistant_detector/main.cc
+++ src/reference/ai_poc/driver_assistant_detector/main.cc
@@ -572,56 +572,14 @@ int main(int argc, char *argv[]) {
 
         std::thread venc_output_thread(venc_output, media.venc_get_channel());
 
-
         while (getchar() != 'q') {
             usleep(10000);
         }
         running = false;
 
         isp_ai_detector_thread.join();
-
-        //fifo_reader_thread.join();
-
         venc_output_thread.join();
     }
-
-
-
-    // example of fifo reader
-    /*std::thread fifo_reader_thread([] {
-        // TODO: DATAFIFO_CMD_GET_AVAIL_READ_LEN does not blocking!
-        while (!isp_stop) {
-            k_u32 readLen = 0;
-            k_s32 s32Ret = kd_datafifo_cmd(hDataFifo[READER_INDEX], DATAFIFO_CMD_GET_AVAIL_READ_LEN, &readLen);
-
-            if (K_SUCCESS != s32Ret)
-            {
-                printf("fifo_reader_thread get available read len error:%x\n", s32Ret);
-                break;
-            }
-
-            if (readLen > 0)
-            {
-                k_char* pBuf;
-                s32Ret = kd_datafifo_read(hDataFifo[READER_INDEX], (void**)&pBuf);
-                if (K_SUCCESS != s32Ret)
-                {
-                    printf("fifo_reader_thread read error:%x\n", s32Ret);
-                    break;
-                }
-                printf("fifo_reader_thread receive:%s\n", pBuf);
-                s32Ret = kd_datafifo_cmd(hDataFifo[READER_INDEX], DATAFIFO_CMD_READ_DONE, pBuf);
-                if (K_SUCCESS != s32Ret)
-                {
-                    printf("fifo_reader_thread read done error:%x\n", s32Ret);
-                    break;
-                }
-            }
-            else {
-                usleep(10000);
-            }
-        }
-    });*/
 
     kd_ipcmsg_disconnect(ipcmsg_handle);
     kd_ipcmsg_del_service(IPCMSG_NAME);
