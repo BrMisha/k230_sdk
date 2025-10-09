b179762719efc9c3482529faa92b26d1decaa6aa
Misha Brazhnikov <br.misha@gmail.com>
2025-10-09 09:47:59 +0300

    updated driver_assistant_detector.sh

--- src/reference/ai_poc/driver_assistant_detector/driver_assistant_detector.sh
+++ src/reference/ai_poc/driver_assistant_detector/driver_assistant_detector.sh
@@ -1,1 +1,1 @@
-./driver_assistant_detector.elf 1 0 best.kmodel 0.65 0.75 0.2 700
+./sharefs/driver_assistant_detector/driver_assistant_detector.elf 1 0 /sharefs/driver_assistant_detector/best.kmodel 0.8 0.85 0.2 700
