ef75cefb2ec0a60ec7bba9d31a6c9db462701947
Michael Brazhnikov <br.misha@gmail.com>
2025-11-30 16:34:38 +0200

    fixed building

--- src/reference/ai_poc/driver_assistant_front/cmake/Riscv64.cmake
+++ src/reference/ai_poc/driver_assistant_front/cmake/Riscv64.cmake
@@ -4,9 +4,17 @@ include(CMakeForceCompiler)
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
-# cross compiler
+# cross compiler (glibc - Xuantie toolchain)
 set(CMAKE_C_COMPILER riscv64-unknown-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER riscv64-unknown-linux-gnu-g++)
+
+# Sysroot from the Xuantie toolchain (has glibc headers)
+set(CMAKE_SYSROOT "/opt/toolchain/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.6.0/sysroot")
+set(CMAKE_C_FLAGS "--sysroot=${CMAKE_SYSROOT}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "--sysroot=${CMAKE_SYSROOT}" CACHE STRING "" FORCE)
+
+# Skip linking during compiler tests
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(CMAKE_C_FLAGS_DEBUG "-O0 -g3")
 set(CMAKE_C_FLAGS_RELEASE "-O0 -s")
