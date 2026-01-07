24bb8795bd511f8e238285894468848c6aaedb99
Michael Brazhnikov <br.misha@gmail.com>
2026-01-07 09:21:41 +0200

    Prevent removing build dir

--- src/reference/ai_poc/driver_assistant_front/build.sh
+++ src/reference/ai_poc/driver_assistant_front/build.sh
@@ -4,8 +4,8 @@ set -x
 # set cross build toolchain
 export PATH=$PATH:/opt/toolchain/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.6.0/bin
 
-rm -rf out
-mkdir out
+# Use incremental build (preserves LVGL build cache)
+mkdir -p out
 pushd out
 cmake -DCMAKE_BUILD_TYPE=Release                 \
       -DCMAKE_INSTALL_PREFIX=`pwd`               \
